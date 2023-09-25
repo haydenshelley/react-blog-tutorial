5f1c361e5a920b09c7b7f2c3e190998c654cb250
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 14:13:16 -0700

    usestate buttons on home page

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log(e);
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name) => {
-    console.log(`hello ${name}`);
+const Home = () => {
+  const [name, setName] = useState("Hayden");
+  const [age, setAge] = useState(29);
+
+  const handleClick = () => {
+    setName("Jaime");
+    setAge(28);
   };
 
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => handleClickAgain("hayden")}>Click Me Again</button>
     </div>
   );
 };
