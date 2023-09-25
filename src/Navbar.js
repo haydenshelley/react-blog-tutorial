1b3612303b09e050e9f6b35f5c418d3a79efe460
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 13:40:19 -0700

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <a href="/">Home</a> | <a href="/create">New Blog</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
