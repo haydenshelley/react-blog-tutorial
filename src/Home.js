af0db8449d4a0cfaf0114b50694a1c1ac5318836
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 14:06:26 -0700

    home page buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log(e);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`hello ${name}`);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClickAgain("hayden")}>Click Me Again</button>
     </div>
   );
 };
