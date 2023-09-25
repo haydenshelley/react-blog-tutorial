8e7c9bb660c579caa30029aa1eaf13ac16abf7f7
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 13:50:30 -0700

    style navbar and homepage

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,10 @@
 const Navbar = () => {
   return (
     <div className="navbar">
+      <h1>Hayden's Blog</h1>
       <div className="links">
-        <a href="/">Home</a> | <a href="/create">New Blog</a>
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
       </div>
     </div>
   );
