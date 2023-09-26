f71807bf57e61d750b2636c2ba186eebd13a159c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 17:28:01 -0700

    delete blog button

--- src/Home.js
+++ src/Home.js
@@ -13,16 +13,23 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} />
+      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === "Mario")}
         title={"Mario's Blogs"}
+        handleDelete={handleDelete}
       />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === "Yoshi")}
         title={"Yoshi's Blogs"}
+        handleDelete={handleDelete}
       />
     </div>
   );
