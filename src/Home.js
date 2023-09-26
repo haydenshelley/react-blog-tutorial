f8cb2dfa8d3d6a09a52a5e72a0bb058d1f52c3b1
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-25 17:06:12 -0700

    map blog posts

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Hayden");
-  const [age, setAge] = useState(29);
-
-  const handleClick = () => {
-    setName("Jaime");
-    setAge(28);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Author: {blog.author} </p>
+        </div>
+      ))}
     </div>
   );
 };
